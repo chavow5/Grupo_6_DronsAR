d9d613b3994d5950f9179bfd2d103e7d4e5262f9
chavow5 <davidramirezw6@gmail.com>
2024-09-19 00:12:36 -0300

    git

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const cookies = require('cookie-parser');
 const userDatasource = require('./services/userDatasource');
 
 
-
 // Rutas de productos
 const dronRouter = require("./routes/dron");
+
 // Rutas de usuarios
 const usersRoutes = require('./routes/users');
 const authMiddleware = require('./middleware/authMiddleware');
@@ -39,9 +39,9 @@ app.use(async (req, res, next) => {
     next();
 });
 
-
 // Definir la ubicación de los archivos estáticos
 app.use(express.static('public'));
+
 // Configuración de EJS
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
@@ -50,51 +50,47 @@ app.use(express.json());
 app.use(methodOverride("_method"));
 
 
-const port = process.env.PORT ?? 3000;
-
 // Proteger rutas con middleware
-// Rutas para autenticación
+// ############# RUTAS ##############
+// Ruta para mostrar la página de login de usuarios, solo para invitados
 app.get('/users/login', authMiddleware.guest, (req, res) => {
     res.render('users/login');
 });
-
+// Ruta para mostrar el perfil del usuario autenticado
 app.get('/users/perfil', authMiddleware.auth, (req, res) => {
     res.render('users/perfil', { user: req.session.user });
 });
-
+// Ruta página principal
 app.get('/', (req, res) => {
     res.render('index');
 });
-
+// Ruta página de login
 app.get('/login', (req, res) => {
     res.render('users/login');
 });
-
+// Ruta página de registro de usuarios
 app.get('/registro', (req, res) => {
     res.render('users/registro');
 });
-
+// Ruta detalle de producto
 app.get('/detalle-producto', (req, res) => {
     res.render('products/detalle-producto');
 });
-
+// Ruta carrito de compras del usuario
 app.get('/carrito-compra', (req, res) => {
     res.render('products/carrito-compra');
 });
-
-// Incluir las rutas del controlador de drones
+// Ruta del controlador de drones
 app.use('/productos', dronRouter);
-
 // Usar las rutas de usuarios
 app.use('/users', usersRoutes);
 
 
-
+//definicion del puerto
+const port = process.env.PORT ?? 3000;
 app.use((req, res) => {
     res.status(404).render("not-found");
   });
-
-
   app.listen(port, (err) => {
     console.log(
         err
